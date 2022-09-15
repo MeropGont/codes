6f8b9d68ba350be0e82335c6a0841ba113898f07
27-apizzuti <a.pizzuti@maastrichtuniversity.nl>
2022-09-15 18:16:18 +0200

    FC and Network analysis

--- FC.m
+++ FC.m
@@ -3,51 +3,58 @@
 clear all;
 clc;
 
-PATH_IN = "C:\Users\apizz\Desktop\DCBT\Project\Data\Data\ROIs";
-PATH_OUT = fullfile('C:\Users\apizz\Desktop\DCBT\Project\Data\Data\FC');
+PATH_IN = "C:\Users\apizz\Desktop\DCBT\Project\Data\Results\ROIs";
+PATH_OUT = fullfile('C:\Users\apizz\Desktop\DCBT\Project\Data\Results\my_FC');
+
 filenames = dir(PATH_IN);
 Nsub=10;
 Ntime=300;
-Nnodes=164;
+Nnodes=132;
 
 if ~exist(PATH_OUT, 'dir')
     mkdir(PATH_OUT)
 end
 Hub_all = zeros(Nsub, Nnodes);
+
 for iterSbj=1:Nsub
+
+    % Loading
     d = load(fullfile(PATH_IN, filenames(iterSbj+2).name));
 
+    % Initializing output
     Data_all = zeros(Ntime, Nnodes);
-    
     Nodes = {};
     FC_struct = struct;
 
-    for iterNodes=1:164
+    for iterNodes=1:132
 
         Data_all(:, iterNodes) = d.data{3+iterNodes};
         Nodes{iterNodes} = d.names{3+iterNodes};
+        coord{iterNodes} = d.xyz{3+iterNodes};
 
     end
 
     FC_mat = corrcoef(Data_all);
 
+    % Compute 'hub'
     degree_mat = nansum(FC_mat, 2); % sum rows
     [max_degree, idx] = max(degree_mat);
+    
     Hub_all(iterSbj, :) = FC_mat(idx, :);
     
-
+    % Save subject-specific result
     FC_struct.data = FC_mat;
     FC_struct.names = Nodes;
     FC_struct.degree = degree_mat;
     FC_struct.Hub = Hub_all;
+    FC_struct.XYZ = coord;
     
-
+    % Plot FC
     imagesc(FC_mat);
-    title(num2str(Nodes{idx}));
-
-    
-
-    pause;
+    colorbar;
+    clim([-1 1])
+    title(['Functional Connectivity (sub-' num2str(iterSbj) ')' ]);
+    saveas(gcf, fullfile(PATH_OUT, ['FC_mat_sub-', num2str(iterSbj)]), 'png');
 
     save(fullfile(PATH_OUT, strcat('FC_matrix_sbj', num2str(iterSbj))), 'FC_mat');
     save(fullfile(PATH_OUT, strcat('FC_struct_sbj', num2str(iterSbj))), 'FC_struct');
@@ -55,6 +62,7 @@ for iterSbj=1:Nsub
 
 end
 
+% Hub-analysis
 x = Hub_all';
 SBJ_corr_hub = corrcoef(x);
 save(fullfile(PATH_OUT, 'AllSbj_hub_corr'), 'SBJ_corr_hub');
---
+++ analysis_results.m
@@ -0,0 +1,93 @@
+clear all;
+clc;
+
+PATH_IN = "C:\Users\apizz\Desktop\DCBT\Project\Data\Results\ROIs";
+PATH_OUT = fullfile('C:\Users\apizz\Desktop\DCBT\Project\Data\Results\my_FC');
+filenames = dir(PATH_IN);
+Nsub=10;
+Ntime=300;
+Nnodes=132;
+
+% check FC matrices computed by conn
+for iterSbj=1:Nsub
+
+    if iterSbj < 10        
+        file1 = strcat('resultsROI_Subject00', num2str(iterSbj), '_Condition001.mat');
+        file2 = strcat('ROI_Subject00', num2str(iterSbj), '_Session001.mat');
+    else
+        file1 = strcat('resultsROI_Subject010_Condition001.mat');
+        file2 = strcat('ROI_Subject010_Session001.mat');
+    end
+
+
+    FCconn = load(fullfile(PATH_IN, '1stAnalysisROIss', file1));
+    Data = load(fullfile(PATH_IN, 'ROIs', file2));
+    
+    % compute in-house FC
+    Data_all = zeros(Ntime, Nnodes);
+        
+    Nodes = {};
+    FC_struct = struct;
+    
+    for iterNodes=1:Nnodes
+    
+        Data_all(:, iterNodes) = Data.data{3+iterNodes};
+        Nodes{iterNodes} = Data.names{3+iterNodes};
+    
+    end
+    
+    FC_mat = corrcoef(Data_all);
+    
+    % Extract FC from conn
+    
+    FC_conn = FCconn.Z(:, 1:Nnodes);
+    
+    % Plot results
+    figure(1);
+    imagesc(FC_mat);
+    title("In-house FC")
+    colorbar;
+    
+    figure(2);
+    FC_conn(FC_conn > 1) = 1;
+    FC_conn(FC_conn < -1) = -1;
+
+    imagesc(FC_conn);
+    title("CONN FC")
+    colorbar;
+    
+    % compute difference
+    diff = FC_mat-FC_conn;
+    figure(3);
+    imagesc(diff);
+    title("in-house FC - CONN FC")
+    colorbar;
+    
+    % Check if the values are correlated in some way
+    vFC_mat = FC_mat(:);
+    vFC_conn = FC_conn(:);
+
+    idx = isnan(vFC_mat);
+    vFC_mat(idx) = 0;
+
+    idx = isnan(vFC_conn);
+    vFC_conn(idx) = 0;
+    
+    % Scatterplot
+
+    figure(4);
+    c = corr(vFC_mat, vFC_conn);
+    plot(vFC_mat);
+    hold on
+    plot(vFC_conn, 'r');
+    title(['Correlation ' num2str(c)])
+    
+
+
+    % pause;
+
+    
+
+end
+
+
